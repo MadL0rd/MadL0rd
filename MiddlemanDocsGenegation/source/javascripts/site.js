68a6699b8bbb04c0e5b578b9915e68385e08597f
Anton Tekutov <anton.tekutow@yandex.ru>
2021-09-27 06:57:42 +0300

    Mobile adaptation test

--- MiddlemanDocsGenegation/source/javascripts/site.js
+++ MiddlemanDocsGenegation/source/javascripts/site.js
@@ -5,4 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
     Array.from(document.getElementsByClassName('progressBar')).forEach(function (element, i) {
         element.setAttribute('data-slick-index', i);
     });
+    Array.from(document.getElementsByClassName('slick-slide')).forEach(function (element, i) {
+        element.setAttribute('data-slick-index', i);
+    });
 }, false);
